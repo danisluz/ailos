7eb3fe330dbdaf45c3727c56d4d576c3d1fb45df
Daniel Luz <29287031+danisluz@users.noreply.github.com>
2023-08-30 14:44:43 -0300

    structring layout

---
+++ src/app/core/layout/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ailos-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
